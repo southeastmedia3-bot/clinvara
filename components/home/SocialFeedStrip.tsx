d0d56ef9dfda97589fe083ee03082aa9daaf8fe3
southeastmedia3 <southeastmedia3@gmail.com>
2026-06-09 14:11:38 +0530

    Adjust social feed scroll speed ag11

--- components/home/SocialFeedStrip.tsx
+++ components/home/SocialFeedStrip.tsx
@@ -260,7 +260,7 @@ export function SocialFeedStrip() {
     const elapsed = currentTime - previousTime;
     previousTime = currentTime;
 
-    track.scrollLeft += 0.8;
+    track.scrollLeft += 0.7;
 
     if (track.scrollLeft >= halfWidth) {
       track.scrollLeft -= halfWidth;
