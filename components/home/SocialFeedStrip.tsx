b68d76697709deac1381d93997fb908f6869a728
southeastmedia3 <southeastmedia3@gmail.com>
2026-06-09 12:49:55 +0530

    Test social scroll speed

--- components/home/SocialFeedStrip.tsx
+++ components/home/SocialFeedStrip.tsx
@@ -260,7 +260,7 @@ export function SocialFeedStrip() {
     const elapsed = currentTime - previousTime;
     previousTime = currentTime;
 
-    track.scrollLeft += elapsed * 0.05;
+    track.scrollLeft += 5;
 
     if (track.scrollLeft >= halfWidth) {
       track.scrollLeft -= halfWidth;
